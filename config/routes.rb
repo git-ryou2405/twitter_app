50852c018bda956d6d862f69f0016d354f400b1b
git-ryou2405 <r2405.exe@gmail.com>
2021-04-06 22:24:22 +0900

    tweet機能だいたい実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'tweets#index'
+  
+  devise_for :users
+  
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+  end
+  
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
 end
